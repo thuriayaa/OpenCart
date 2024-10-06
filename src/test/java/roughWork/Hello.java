f2e0b151bda6ef91eae73e33bb4f155891df53cd
thuriayaa <149255459+thuriayaa@users.noreply.github.com>
2024-10-06 12:42:10 -0400

    Update Hello.java
    no need for this... :D

--- src/test/java/roughWork/Hello.java
+++ src/test/java/roughWork/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		
 		System.out.println("next session is the last session for automation. YES. finally. I am so happy. Alhamdulillah. ");
 
-		//we developed this class and almost to the end of the automation.
+		//we developed this class and almost to the end of the automation. I miss Mohammad so much :'( 
 	
 	}
 	
